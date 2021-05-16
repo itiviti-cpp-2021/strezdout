0e4f9dc09a93ad03479188209e73a3e30a22bf23
renbou <artemmikheev1311@gmail.com>
2021-05-16 16:47:50 +0300

    Change tested fibonacci source

--- resources/testers/fibonacci/src/fibonacci.cpp
+++ resources/testers/fibonacci/src/fibonacci.cpp
@@ -1,4 +1,15 @@
 #include "fibonacci.h"
+#include <vector>
+
+// int fibonacci(int n) {
+//   if (n <= 0) {
+//     return 0;
+//   }
+//   if (n == 1 || n == 2) {
+//     return 1;
+//   }
+//   return fibonacci(n - 1) + fibonacci(n - 2);
+// }
 
 int fibonacci(int n) {
   if (n <= 0) {
@@ -7,5 +18,10 @@ int fibonacci(int n) {
   if (n == 1 || n == 2) {
     return 1;
   }
-  return fibonacci(n - 1) + fibonacci(n - 2);
+  std::vector<int> fib(n + 1);
+  fib[1] = fib[2] = 1;
+  for (int i = 3; i <= n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib[n];
 }
